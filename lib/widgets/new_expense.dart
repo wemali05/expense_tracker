2054cb4f9bd648226f6de4523a23954c62543748
weali05 <alfredwemali05@gmail.com>
2024-11-25 12:17:53 +0300

    number input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -11,10 +12,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _textController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _textController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -31,11 +34,25 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly,],
+            decoration: const InputDecoration(
+              prefixText: '\$ ',
+              label: Text('Amount'),
+            ),
+          ),
           Row(
             children: [
+              TextButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              ),
               ElevatedButton(
                 onPressed: () {
-                  print(_textController);
+                  print(_textController.text);
+                  print(_amountController.text);
                 },
                 child: const Text('Save Example'),
               ),
