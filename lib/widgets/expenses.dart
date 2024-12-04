21b11a36faf5d106b521a4f75d93825dcbd7252e
weali05 <alfredwemali05@gmail.com>
2024-12-04 07:12:55 +0300

    dissmissible widget for dismissing list items

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -29,13 +29,21 @@ class _ExpensesState extends State<Expenses> {
     showModalBottomSheet(
       isScrollControlled: true,
       context: context,
-      builder: (ctx) => NewExpense(onAddExpense: _addExpense,),
+      builder: (ctx) => NewExpense(
+        onAddExpense: _addExpense,
+      ),
     );
   }
 
   void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
     });
   }
 
@@ -55,7 +63,10 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('Title here'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
           ),
         ],
       ),
