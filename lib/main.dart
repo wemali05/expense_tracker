2314bfee3eb9123097d3606446ce1745250410f3
weali05 <alfredwemali05@gmail.com>
2024-12-05 21:46:16 +0300

    text theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
-var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 95, 59, 181));
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 95, 59, 181));
 void main() {
   runApp(
-     MaterialApp(
+    MaterialApp(
       theme: ThemeData().copyWith(
         // useMaterial3: true,
         scaffoldBackgroundColor: const Color.fromARGB(255, 153, 153, 228),
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer
-        )
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 14,
+              ),
+            ),
       ),
       home: const Expenses(),
     ),
