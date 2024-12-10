572e0de7a5d98059330513de7721bcfee0e11bb7
weali05 <alfredwemali05@gmail.com>
2024-12-10 13:10:47 +0300

    dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,34 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
-var kColorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 95, 59, 181));
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 95, 59, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        // useMaterial3: true
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         // useMaterial3: true,
         scaffoldBackgroundColor: const Color.fromARGB(255, 153, 153, 228),
@@ -33,6 +56,7 @@ void main() {
               ),
             ),
       ),
+      // themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
