eb05a81e8b66ae5599044d6f0c94fd62f25f9884
weali05 <alfredwemali05@gmail.com>
2024-12-18 18:15:30 +0300

    understaning 'safe area'

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -28,6 +28,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpensesOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(
