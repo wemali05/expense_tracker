c00591d91a31155116751a98a42c42f9e8fb1de4
weali05 <alfredwemali05@gmail.com>
2024-12-18 10:24:57 +0300

    lock screen orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 95, 59, 181),
@@ -10,6 +11,12 @@ var kDarkColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 5, 99, 125),
 );
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]).then((fn) {
+
+  });
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
